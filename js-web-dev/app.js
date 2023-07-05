583b51b2188cb21e9edbd02b4a60afe601c1ceaf
Aurnab Das <aurnabdas@Aurnabs-MacBook-Pro.local>
2023-07-05 19:02:26 -0400

    test 1

--- js-web-dev/app.js
+++ js-web-dev/app.js
@@ -1,11 +1,19 @@
 const express = require('express')
+const path = require('path');
 
 const app = express();
 
 const PORT = 3000;
 
-app.use(express.static('static'));
-app.use('/images', express.static('images'));
+
+app.get('/movie/:name', function(req, res) {
+    // res.sendFile(path.join(__dirname, '/static/frontpage.html')); //static
+    var movie = req.params.name; // attempt to make a api call to get movie id
+    res.set('Content-Type', 'text/html');
+    res.send(Buffer.from('<h2>Test ' + movie + ' </h2>'));
+  });
+// app.use(express.static('static'));
+// app.use('/images', express.static('images'));
 
 app.listen(PORT, ()=> {
     console.log(`App listen on port ${PORT}...`);
